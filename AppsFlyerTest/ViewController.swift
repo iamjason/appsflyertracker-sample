214ab3902bf1f5b33f6de17af37151cb1bf4bfd4
iamjason <jasongarrett@Jasons-MacBook-Pro.local>
2015-05-14 14:48:53 -0400

    sample app commit

---
+++ AppsFlyerTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AppsFlyerTest
+//
+//  Created by Jason Garrett on 5/14/15.
+//  Copyright (c) 2015 jg. All rights reserved.
+//
+
+import UIKit
+import AppsFlyerTracker
+
+class ViewController: UIViewController {
+
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
